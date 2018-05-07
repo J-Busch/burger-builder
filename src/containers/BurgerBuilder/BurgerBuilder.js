1f059a0213ebde1fe678a6f1117258744b351f60
J-Busch <joelbusch94@gmail.com>
2018-05-07 14:43:50 -0500

    added desktop navigation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -72,6 +72,12 @@ class BurgerBuilder extends Component {
 	purchaseHandler = () => {
 		this.setState({ purchasing: true });
 	}
+	purchaseCancelHandler = () => {
+		this.setState({ purchasing: false });
+	}
+	purchaseContinueHandler = () => {
+		alert('You continue!');
+	}
 
 	render() {
 		const disabledInfo = { ...this.state.ingredients };
@@ -81,9 +87,12 @@ class BurgerBuilder extends Component {
 
 		return (
 			<Aux>
-				<Modal show={this.state.purchasing}>
+				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
 					<OrderSummary
 						ingredients={this.state.ingredients}
+						price={this.state.totalPrice}
+						purchaseCanceled={this.purchaseCancelHandler}
+						purchaseContinued={this.purchaseContinueHandler}
 					/>
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
